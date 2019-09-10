aabf395b2d492d3fb7169f5390404014f42a07b8
Waldir Marques <waldir.marques@dcx.ufpb.br>
2019-09-10 13:40:13 -0300

    criação da classe DBService, instalação de dependencia e de application dev e test

---
+++ src/main/java/com/br/waldir/services/DBService.java
@@ -0,0 +1,48 @@
+package com.br.waldir.services;
+
+import java.text.ParseException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.waldir.domain.CategoriaFilme;
+import com.br.waldir.domain.Filme;
+import com.br.waldir.domain.Usuario;
+import com.br.waldir.repositories.CategoriaFilmeRepository;
+import com.br.waldir.repositories.FilmeRepository;
+import com.br.waldir.repositories.UsuarioRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private CategoriaFilmeRepository categoriaFilmeRepository;
+	@Autowired
+	private FilmeRepository filmeRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	public void instantiateTestDatabase() throws ParseException {
+
+Usuario user1 = new Usuario(null, "Waldir", "waldir.marques@dcx.ufpb.br", "12345678");
+		
+		CategoriaFilme cf1 = new CategoriaFilme(null,"Ação");
+		CategoriaFilme cf2 = new CategoriaFilme(null,"Terror");
+		
+		Filme f1 = new Filme(null,"Veloses e Furiosos",true,"23/07/2019");
+		Filme f2 = new Filme(null,"Boneca assasina",false,null);
+		Filme f3 = new Filme(null,"Motoqueiro Fantasma",false,null);
+		
+		cf1.getFilmes().addAll(Arrays.asList(f1));
+		cf2.getFilmes().addAll(Arrays.asList(f2,f3));
+		
+		f1.getCategoriaFilmes().addAll(Arrays.asList(cf1));
+		f1.getCategoriaFilmes().addAll(Arrays.asList(cf2));
+		
+		usuarioRepository.saveAll(Arrays.asList(user1));
+		categoriaFilmeRepository.saveAll(Arrays.asList(cf1,cf2));
+		filmeRepository.saveAll(Arrays.asList(f1,f2));
+		
+	}
+}
